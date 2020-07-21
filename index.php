a47f6329963536ac509a9685dc0dcaa2d0c28d08
yuxcat <56475797+yuxcat@users.noreply.github.com>
2020-07-22 01:25:57 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+</head>
+<body>
+
+<form action="test.php" method="POST" name="dates">
+<label for="checkin">Check In Date:</label>
+<input type="date" name="checkin">
+ <label for="checkout">Check Out Date:</label>
+<input type="date" name="checkout">
+
+</body>
+</html>
