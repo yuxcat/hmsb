cb3807d90fb6bba5ff6eed8b8d2e0b6f85e0c78f
yuxcat <56475797+yuxcat@users.noreply.github.com>
2020-07-30 03:22:31 +0530

    Update booking.php

--- booking.php
+++ booking.php
@@ -4,23 +4,22 @@ echo $_POST["checkout"];
 echo $_POST["roomsdd"];
 echo $_POST["adultsdd"];
 echo $_POST["lildd"];
-
 /* @mysql : error hiding : use mySQL or PDO since mysql extention will remove in the future */
 $con = @mysql_connect("localhost", "root", "");
 if (!$con)
 {
     die('Could not connect: ' . mysql_error());
 }
-mysql_select_db("roomdb", $con);
-$sql = "INSERT INTO rooms (checkin, checkout, roomq, adults, lils)
+mysql_select_db("hms", $con);
+$sql = "INSERT INTO bookings (room_no, checkin, checkout, rooms, adults, lils)
 VALUES
-('$_POST[checkin]','$_POST[checkout]','$_POST[roomsdd]','$_POST[adultsdd]','$_POST[lildd]')";
+('$_POST[slct]','$_POST[checkin]','$_POST[checkout]','$_POST[roomsdd]','$_POST[adultsdd]','$_POST[lildd]')";
 if (!mysql_query($sql, $con))
 {
     die('Error: ' . mysql_error());
 }
 
-header('Location: order.php');
+
 echo "1 record added";
 mysql_close($con)
 ?>
